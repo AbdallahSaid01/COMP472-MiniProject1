79529b1d7673409e45b62e11e2003f807256fbf8
AbdallahSaid01 <59895483+AbdallahSaid01@users.noreply.github.com>
2022-10-21 19:53:46 -0400

    Update Parts 2.3.3. and 2.3.6 in 2.5.py

--- Parts 2.3.3. and 2.3.6 in 2.5.py
+++ Parts 2.3.3. and 2.3.6 in 2.5.py
@@ -24,8 +24,8 @@ y1 = split[:, 1]
 y2 = split[:, 2]
 
 # Part 2.2
-x1_train, x1_test, y1_train, y1_test = train_test_split(x, y1, train_size=0.8, test_size=0.2)
-x2_train, x2_test, y2_train, y2_test = train_test_split(x, y2, train_size=0.8, test_size=0.2)
+x1_train, x1_test, y1_train, y1_test = train_test_split(x, y1, train_size=0.5, test_size=0.5)
+x2_train, x2_test, y2_train, y2_test = train_test_split(x, y2, train_size=0.5, test_size=0.5)
 
 #part 2.5
 
