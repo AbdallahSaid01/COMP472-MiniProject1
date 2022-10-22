d6d4d70cd2fcd186e7c901ca84f3eebed4da956c
AbdallahSaid01 <59895483+AbdallahSaid01@users.noreply.github.com>
2022-10-21 20:03:52 -0400

    Added 3.5 to 3.7

--- Part 3.1-3.7.py
+++ Part 3.1-3.7.py
@@ -6,8 +6,7 @@ from nltk import word_tokenize
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import classification_report, confusion_matrix
-from sklearn.feature_extraction._dict_vectorizer import DictVectorizer
-
+from sklearn.model_selection import GridSearchCV
 
 model = api.load('word2vec-google-news-300')
 
@@ -47,3 +46,38 @@ print("Embedding done and printing array")
 print("Calculating hit and miss rates")
 hitRate = (totalWordsFound/(totalWordsFound+totalWordsNotFound))*100
 missRate = (totalWordsNotFound/(totalWordsFound+totalWordsNotFound))*100
+
+x1_train, x1_test, y1_train, y1_test = train_test_split(sentenceEmbed, y1, train_size=0.8, test_size=0.2)
+x2_train, x2_test, y2_train, y2_test = train_test_split(sentenceEmbed, y2, train_size=0.8, test_size=0.2)
+
+#3.5
+
+perceptron_classifier = MLPClassifier(max_iter=5)
+print("We are using a Multi-Layered Perception Classifier or MLPClassifier with the default parameters.")
+perceptron_classifier.fit(x1_train, y1_train)
+y1_pred = perceptron_classifier.predict(x1_test)
+print("Emotions: \n")
+print(classification_report(y1_test, y1_pred))
+print("Confusion Matrix: \n", confusion_matrix(y1_test, y1_pred))
+perceptron_classifier = MLPClassifier(max_iter=20)
+perceptron_classifier.fit(x2_train, y2_train)
+y2_pred = perceptron_classifier.predict(x2_test)
+print("Sentiments: \n")
+print(classification_report(y2_test, y2_pred))
+print("Confusion Matrix: \n", confusion_matrix(y2_test, y2_pred))
+
+#3.6
+
+# print("We are using a Multi-Layered Perception Classifier or MLPClassifier with the hyperparameters.")
+# perceptron_classifier = MLPClassifier(max_iter=20, activation='relu', hidden_layer_sizes=(30,50), solver='sgd')
+# print("We are using a Multi-Layered Perception Classifier or MLPClassifier with GridSearchCV.")
+# perceptron_classifier.fit(x1_train,y1_train)
+# y1_pred = perceptron_classifier.predict(x1_test)
+# print("Emotions: \n")
+# print(classification_report(y1_test,y1_pred))
+# print("Confusion Matrix: \n")
+#
+# perceptron_classifier.fit(x2_train,y2_train)
+# y2_pred = perceptron_classifier.predict(x2_test)
+# print("Sentiments: \n")
+# print(classification_report(y2_test, y2_pred))
